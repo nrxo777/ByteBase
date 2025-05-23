8b9df1921106c5610d46b1abf8fd66ad3d52f639
nrxo777 <noorxcalma@gmail.com>
2025-05-24 00:12:01 +0600

    Refactor API endpoint and enhance joke fetching functionality; update title and add axios dependency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { use, useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) => {
+      setJokes(response.data)
+    }) 
+    .catch((error) => {
+      console.error('Error fetching jokes:', error)
+    })
+  })
 
   return (
-    <h1>Byte Base</h1>
+    <>
+      <h1>Joke List</h1>
+      <h2>Here's 5 Jokes: {jokes.length}</h2>
+      <hr />
+
+      {
+        jokes.map((joke, index) => (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
+    </>
   )
 }
 
